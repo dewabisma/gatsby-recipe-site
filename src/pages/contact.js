7aa6a8eea021775e872c7b775ffe12d47a2d58b8
Dewa Bisma <idewagedebisma@gmail.com>
2021-05-15 12:57:31 +0800

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,51 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout>
-      <h1>From Contact Page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to Get in Touch?</h3>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              quos debitis nemo exercitationem inventore quo laudantium, alias
+              repellendus illo numquam?
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Mollitia, voluptatum.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsa
+              saepe, labore est accusamus itaque.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+
+              <div className="form-row">
+                <label htmlFor="email">Your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+
+              <div className="form-row">
+                <label htmlFor="message">Your message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+
+              <button className="btn block" type="submit">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
