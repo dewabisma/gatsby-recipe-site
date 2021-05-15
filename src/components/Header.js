bb5a909616b0749c4589405540ea5a01c1ea162d
Dewa Bisma <idewagedebisma@gmail.com>
2021-05-15 13:13:21 +0800

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FaBars } from "react-icons/fa"
+
+import logo from "../assets/images/logo.svg"
 
 const Header = () => {
   return (
@@ -7,8 +10,35 @@ const Header = () => {
       <div className="nav-center">
         <div className="nav-header">
           <Link to="/">
-            <img src="" alt="simply recipes logo" />
+            <img src={logo} alt="simply recipes logo" />
           </Link>
+
+          <button className="nav-btn">
+            <FaBars />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            Recipes
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            Tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
